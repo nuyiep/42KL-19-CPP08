940e886fc16d76b3c3fce3b0a4ee7b1b27ccb767
Pei Yun Lau <plau@u91z08s01.42kl.edu.my>
2023-08-01 13:55:10 +0800

    ex00- basic done

--- ex00/srcs/easyfind.hpp
+++ ex00/srcs/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: plau <plau@student.42.kl>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 13:24:58 by plau              #+#    #+#             */
-/*   Updated: 2023/07/31 18:54:36 by plau             ###   ########.fr       */
+/*   Updated: 2023/08/01 13:54:34 by plau             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@
 #include <map>
 #include <vector>
 #include <list>
+#include <algorithm>
+#include "color.hpp"
 
 /* Exception class */
 class noOccurrenceIsFound : public std::exception
@@ -29,18 +31,42 @@ class noOccurrenceIsFound : public std::exception
 	
 };
 
+/* Use S/Users/plau/19-CPP07/ex02/srcs/color.hpp/Users/plau/19-CPP07/ex02/srcs/color.hpp/Users/plau/19-CPP07/ex02/srcs/color.hppTL algorithms for searching */
+/* data.end() returns an iterator pointing beyond the last element */
+/* if std::find does not find the target, it returns data.end() */
+/* check if x is found */
+/* std::distance- calculates the distance between 2 iterators */
+/* 				- beginning iterator and found iterator */
 template<typename T>
 int easyfind(T data, int x)
 {
-	std::list<int>::iterator it1;
-	int	i = 1;
-	for (it1 = data.begin(); it1 != data.end(); ++it1)
+	typename T::iterator it1;
+	
+	it1 = std::find(data.begin(), data.end(), x);
+	if (it1 != data.end()) //element is found
 	{
-		if (*it1 == x)
-			return (i);
-		i++;
+		int pos = std::distance(data.begin(), it1);
+		return (pos + 1);
 	}
-	throw noOccurrenceIsFound();
+	else
+	{
+		throw noOccurrenceIsFound();
+	}
 }
+
+/* Manual search using iterators - Not allowed by subject PDF */
+// template<typename T>
+// int easyfind(T data, int x)
+// {
+// 	std::list<int>::iterator it1;
+// 	int	i = 1;
+// 	for (it1 = data.begin(); it1 != data.end(); ++it1)
+// 	{
+// 		if (*it1 == x)
+// 			return (i);
+// 		i++;
+// 	}
+// 	throw noOccurrenceIsFound();
+// }
 
 #endif
--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: plau <plau@student.42.kl>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 13:25:01 by plau              #+#    #+#             */
-/*   Updated: 2023/07/31 18:52:50 by plau             ###   ########.fr       */
+/*   Updated: 2023/08/01 13:54:47 by plau             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,15 @@
 
 int main()
 {
-	int x = 19;
+	int x = 1;
+	
 	std::list<int> list1;
 	list1.push_back(199);
 	list1.push_back(1);
 	list1.push_back(19);
+
 	std::list<int>::iterator it1;
+	std::cout << "List: ";
 	for (it1 = list1.begin(); it1 != list1.end(); ++it1)
 	{
 		std::cout << *it1 << " ";
@@ -28,7 +31,8 @@ int main()
 	try
 	{
 		int i = easyfind(list1, x);
-		std::cout << "First occurence of " << x << " is at " << "position " << i << std::endl;
+		std::cout << "PS: Position starts from 1, not 0" << std::endl;
+		std::cout << BOLD_GREEN << "First occurence of " << x << " is at " << "position " << i << RESET << std::endl;
 	}
 	catch(const std::exception& e)
 	{
