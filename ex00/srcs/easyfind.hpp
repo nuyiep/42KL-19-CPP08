5342a6421746f8ae3541f34253419185b8f6d6a8
Pei Yun Lau <plau@u91z08s01.42kl.edu.my>
2023-08-01 18:12:29 +0800

    ex00 done- if return value is template T::iterator, code will be simpler

--- ex00/srcs/easyfind.hpp
+++ ex00/srcs/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: plau <plau@student.42.kl>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 13:24:58 by plau              #+#    #+#             */
-/*   Updated: 2023/08/01 13:54:34 by plau             ###   ########.fr       */
+/*   Updated: 2023/08/01 18:02:39 by plau             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@
 #include <map>
 #include <vector>
 #include <list>
-#include <algorithm>
+#include <array>
+#include <algorithm> //find
 #include "color.hpp"
 
 /* Exception class */
@@ -26,17 +27,19 @@ class noOccurrenceIsFound : public std::exception
 	public:
 		const char *what() const throw()
 		{
-			return ("No occurence is found");
+			return (BOLD_RED "No occurence is found" RESET);
 		}
-	
 };
 
-/* Use S/Users/plau/19-CPP07/ex02/srcs/color.hpp/Users/plau/19-CPP07/ex02/srcs/color.hpp/Users/plau/19-CPP07/ex02/srcs/color.hppTL algorithms for searching */
 /* data.end() returns an iterator pointing beyond the last element */
 /* if std::find does not find the target, it returns data.end() */
 /* check if x is found */
 /* std::distance- calculates the distance between 2 iterators */
 /* 				- beginning iterator and found iterator */
+/* [Typename] need because complier doesnt know what T::iterator is */
+/* 			- it could be a function or member */
+/*			- T::iterator is a dependent name because it depends on */
+/*			  the template parameter T */
 template<typename T>
 int easyfind(T data, int x)
 {
@@ -49,9 +52,7 @@ int easyfind(T data, int x)
 		return (pos + 1);
 	}
 	else
-	{
 		throw noOccurrenceIsFound();
-	}
 }
 
 /* Manual search using iterators - Not allowed by subject PDF */
--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -6,21 +6,29 @@
 /*   By: plau <plau@student.42.kl>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 13:25:01 by plau              #+#    #+#             */
-/*   Updated: 2023/08/01 13:54:47 by plau             ###   ########.fr       */
+/*   Updated: 2023/08/01 17:45:40 by plau             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 
+/* std::vector constructor takes 2 iterators (begin and end) */
+/* is used to initialize vector1 with the elements from the array */
+/* arrayOne - starting address of the array */
+/* arrayOne + number of elements in the array */
 int main()
 {
-	int x = 1;
+	int x = 11;
+	int i = 0;
 	
+	std::cout << BOLD_MAGENTA << "*** Containers ***" << RESET << std::endl;
+	/* List */
+	std::cout << BOLD_MAGENTA << "*** List ***" << RESET << std::endl;
 	std::list<int> list1;
 	list1.push_back(199);
-	list1.push_back(1);
+	list1.push_back(11);
 	list1.push_back(19);
-
+	
 	std::list<int>::iterator it1;
 	std::cout << "List: ";
 	for (it1 = list1.begin(); it1 != list1.end(); ++it1)
@@ -28,11 +36,65 @@ int main()
 		std::cout << *it1 << " ";
 	}
 	std::cout << std::endl;
+	std::cout << "toFind: " << x << std::endl;
 	try
 	{
-		int i = easyfind(list1, x);
-		std::cout << "PS: Position starts from 1, not 0" << std::endl;
+		i = easyfind(list1, x);
+		/* PS: Position starts from 1, not 0 */
+		std::cout << BOLD_GREEN << "First occurence of " << x << " is at " << "position " << i << RESET << std::endl << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	/* Vector */
+	std::cout << BOLD_MAGENTA << "*** Vector ***" << RESET << std::endl;
+	int arrayOne[4] = {55, 66, 99, 33};
+	int numberOfElements = sizeof(arrayOne) / sizeof(arrayOne[0]);
+	std::cout << "Number of elements in vector: " << numberOfElements << std::endl;
+	std::cout << "Vector: ";
+	std::vector<int> vectorOne(arrayOne, arrayOne + numberOfElements);
+	std::vector<int>::iterator it2;
+	for (it2 =vectorOne.begin(); it2 != vectorOne.end(); ++it2)
+	{
+		std::cout << *it2 << " ";
+	}
+	std::cout << std::endl;
+	x = 99;
+	std::cout << "toFind: " << x << std::endl;
+	try
+	{
+		i = easyfind(vectorOne, x);
 		std::cout << BOLD_GREEN << "First occurence of " << x << " is at " << "position " << i << RESET << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	/* Array */
+	/* Array won't work because int* is a pointer type */
+	/* and it doesnt have a member type called iterator */
+	/* Need to use a container type that has an iterator */
+	std::cout << std::endl << BOLD_MAGENTA << "***** Array *****" << RESET << std::endl;
+	int			toFind = 1000;
+	// int			array1[4] = {5, 6, 9, 3};
+	//const int	array2[5] = {42, 5, 16, 2022, -99};
+	const size_t size = 4;
+	std::array<int, size>arrayTwo = {5, 6, 9, 3};
+	std::cout << "Array: ";
+	std::array<int, size>::iterator it3;
+	for (it3 = arrayTwo.begin(); it3 != arrayTwo.end(); ++it3)
+	{
+		std::cout << *it3 << " ";
+	}
+	std::cout << std::endl;
+	std::cout << "To find: " << toFind << std::endl;
+	try
+	{
+		i = easyfind(arrayTwo, toFind);
+		std::cout << BOLD_GREEN << "First occurence of " << toFind << " is at " << "position " << i << RESET << std::endl << std::endl;
 	}
 	catch(const std::exception& e)
 	{
